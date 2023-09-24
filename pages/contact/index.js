bc5f466b1c600cdf19f1a4aef378f2459aa906ba
narendra2895 <106621535+narendra2895@users.noreply.github.com>
2023-09-24 18:04:58 +0100

    contact form working commit

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,17 +1,20 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef,useState } from 'react';
 import * as THREE from 'three';
 import Image from "next/image";
+import emailjs from 'emailjs-com';
+
 
 // framermotion
 
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
 // variants
-import {fadeIn} from '../../variants';
+import { fadeIn } from '../../variants';
 
 
 const Contact = () => {
   const containerRef = useRef(null);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
   useEffect(() => {
     // Three.js code
@@ -103,13 +106,33 @@ const Contact = () => {
     };
   }, []);
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_wyvebdx', 'template_tjiwauq', e.target, 'BV-7QcPe5yG3wbFeT')
+      .then((result) => {
+        console.log(result.text);
+        setIsFormSubmitted(true)
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
+
   return (
     <div ref={containerRef} className="w-full h-50 bg-black">
+        {isFormSubmitted ? ( // Conditional rendering based on form submission status
+        <div className="container absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-opacity-60 bg-black p-8 rounded-2xl text-center">
+          <h1 className="h1 text-white font-semibold mb-4">
+            Thanks for reaching out to me, I will <span className='text-accent'>connect with you shortly</span>
+          </h1>
+        </div>
+      ) : (
       <div id="contact-form" className="container absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-opacity-60 bg-black p-8 rounded-2xl ">
         <h1 className="h1 text-center font-semibold mb-4">Reach Me to <span className='text-accent'>Hire Me.</span></h1>
-        <form className='text-black flex xl:gap-x-10'>
+        <form className='text-black flex xl:gap-x-10' onSubmit={sendEmail}>
           <motion.div variants={fadeIn('right', 0.3)} initial="hidden" animate="show" exit="hidden" className='h-full '>
-            <Image src={'images/contact-avatar.png'} width={400} height={400} alt="avatar" className='translate-z-0 hidden xl:block rounded-3xl' />
+            <Image src={'/contact-avatar.png'} width={400} height={400} alt="avatar" className='translate-z-0 hidden xl:block rounded-3xl' />
           </motion.div>
           <div className='xl:w-[800px] w-full'>
             <motion.div variants={fadeIn('left', 0.3)} initial="hidden" animate="show" exit="hidden" className="mb-4">
@@ -124,12 +147,11 @@ const Contact = () => {
               <label htmlFor="message" className="block font-medium text-white">Message:</label>
               <textarea id="message" name="message" className="w-full px-4 py-2 border rounded" rows="4" required />
             </motion.div>
-            <motion.button variants={fadeIn('up', 0.1)} initial="hidden" animate="show" exit="hidden" type="submit" className="bg-orange-300 hover:bg-accent  text-black hover:text-white htransform hover:scale-105 transition-transform duration-300 ease-in-out font-semibold py-2 px-4 rounded">
-              Submit
-            </motion.button>
+            <motion.input variants={fadeIn('up', 0.1)} initial="hidden" animate="show" exit="hidden" type="submit" value="Send" className="bg-orange-300 hover:bg-accent  text-black hover:text-white htransform hover:scale-105 transition-transform duration-300 ease-in-out font-semibold py-2 px-4 rounded" />
+             
           </div>
         </form>
-      </div>
+      </div>)}
     </div >
   );
 };
