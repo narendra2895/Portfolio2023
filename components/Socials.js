7025245c2d6cf9f311af04b2c1f72468fc1d08e0
narendra2895 <106621535+narendra2895@users.noreply.github.com>
2024-02-03 04:08:32 +0000

    version-feb.3

--- components/Socials.js
+++ components/Socials.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
 
-import {RiInstagramLine, RiWhatsappLine, RiLinkedinLine} from 'react-icons/ri'
+import { RiInstagramLine, RiWhatsappLine, RiLinkedinLine } from 'react-icons/ri'
 
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-3xl">
-      <Link href={''} className="hover:text-accent transition-all duration-300" ><RiInstagramLine /></Link>
-      <Link href={''} className="hover:text-accent transition-all duration-300" ><RiWhatsappLine /></Link>
-      <Link href={''} className="hover:text-accent transition-all duration-300" ><RiLinkedinLine /></Link>
+      <Link href={'https://www.instagram.com/narendra_kulkarni28/'} target="_blank" className="hover:text-accent transition-all duration-300" ><RiInstagramLine /></Link>
+      <Link href={'https://wa.me/7039226256'} target="_blank" className="hover:text-accent transition-all duration-300" ><RiWhatsappLine /></Link>
+      <Link href={'https://www.linkedin.com/in/narendra-kulkarni-4a3487216/'} target="_blank" className="hover:text-accent transition-all duration-300" ><RiLinkedinLine /></Link>
+
     </div>
   );
 };
